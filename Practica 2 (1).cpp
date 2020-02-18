aa88751559923717a03e38a827f149f2bd5c1e7c
josepineda26 <60158079+josepineda26@users.noreply.github.com>
2020-02-18 11:58:53 -0600

    Add files via upload

---
+++ Practica 2 (1).cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+float a,b,c;
+int opt;
+
+printf(" 1.- Suma \n 2.- Resta \n 3.- Multiplicacion \n 4.- Division");
+printf(" \n Seleccione una opcion: ");
+scanf("%f",&opt);
+printf("Ingrese el primer operando: ");
+scanf("%f",&a);
+printf("Ingrese el segundo operando: ");
+scanf("%f", &b);
+switch(opt){
+case 1:
+    c=a+b;
+    break;
+case 2:
+    c=a-b;
+    break;
+case 3:
+    c=a*b;
+    break;
+case 4:
+    while (b==0){
+        printf("Error, b debe ser distinto de 0\n");
+        printf("Introduce el segundo operando:");
+        scanf("%f", &b);
+    }
+
+    c=a/b;
+    break;
+
+    default:
+
+    printf("Error, opcion invalida");
+    c=0;
+}
+   
+    printf("El resultado es %f", c);
+}
